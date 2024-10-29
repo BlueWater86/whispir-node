de989d35805aa737a4a909fd59469fbf6acd151a
BlueWater86 <daniel@ahcomputers.com.au>
2024-10-29 22:42:36 +1000

    Delete dist directory

--- dist/version.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = void 0;
-// x-release-please-start-version
-exports.VERSION = '1.1.0';
-// x-release-please-end
